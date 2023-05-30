46cbe397e02541aa0c4b681257d603703ec73028
Dmitry Frolov <dmitsf@gmail.com>
2023-05-30 11:31:30 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+import math
+import multiprocessing as mp
+import time
+
+
+def cpu_load_task():
+    while True:
+        _ = math.sqrt(64*64*64*64*64)
+
+
+TIMEOUT = 180
+
+if __name__ == '__main__':
+    pool = mp.Pool()
+    results = [pool.apply_async(cpu_load_task) for _ in range(4)]
+    time.sleep(TIMEOUT)
+    pool.terminate()
